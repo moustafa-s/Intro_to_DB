15a6888f72a1d9ccc735b4484007c8734ea7e341
moustafa-s <108592926+moustafa-s@users.noreply.github.com>
2024-10-25 10:35:02 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,11 +15,11 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        if (connection.is_connected()):
+        if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
             print("MySQL connection is closed.")
